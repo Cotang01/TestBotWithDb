f97f070cf6ae061ed81eefb570aa9e12eca546ee
Sasha <Geimer0073@yandex.ru>
2025-02-28 23:37:40 +0500

    mv commands into module

---
+++ commands.py
@@ -0,0 +1,7 @@
+from aiogram.types import BotCommand
+
+
+base_commands = [
+    BotCommand(command='/start', description='Запуск бота'),
+    BotCommand(command='/show_items', description='Показать предметы'),
+]
